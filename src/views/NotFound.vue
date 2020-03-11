37a6211b0d9e1ce47a3a74da4151498fea67f6cc
Chauncey <18322276773@163.com>
2020-03-11 16:50:23 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        您要找的页面走丢啦!!!
+        <span><router-link to="/">返回首页</router-link></span>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
