1d3cced627bca0f808845a4075c88f06676242cb
Chauncey <18322276773@163.com>
2020-03-12 16:36:35 +0800

    完成numberpad和notes组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <label class="notes">
-      <span class="name">备注</span>
-      <input type="text" placeholder="在这里输入备注">
+      <span class="name">{{value}}备注</span>
+      <input :value="value" @input="value = $event.target.value" type="text" placeholder="在这里输入备注">
     </label>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Notes'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  @Component
+  export default class Notes extends Vue{
+    value = '';
+  }
 </script>
 
 <style lang="scss" scoped>
