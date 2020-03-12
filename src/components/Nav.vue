e9d07328e70559d921028c136e9bf70dd6ffeb1c
Chauncey <18322276773@163.com>
2020-03-12 10:31:14 +0800

    添加了全局样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,6 +22,7 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "~@/assets/styles/helper.scss";
     nav{
         display: flex;
         box-shadow: 0 0 3px rgba(0,0,0,.25);
@@ -40,7 +41,7 @@
             }
         }
         >.item.selected{
-            color: red;
+            color: $color-height;
         }
     }
 </style>
